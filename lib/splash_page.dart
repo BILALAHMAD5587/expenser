df1d991d43ebae6aacccd6539baa2bb72108862e
Flutter <Bilalmalik5587@gmail.com>
2023-02-05 17:58:53 +0500

    expenser_app

---
+++ lib/splash_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Splashp_Page extends StatefulWidget {
+  const Splashp_Page({Key? key}) : super(key: key);
+
+  @override
+  State<Splashp_Page> createState() => _Splashp_PageState();
+}
+
+class _Splashp_PageState extends State<Splashp_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
