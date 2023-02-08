c3e576dca8c9699023163f7bf850d86e89a8f0d5
BILALAHMAD5587 <bilalmalik5587@gmail.com>
2023-02-08 10:15:07 +0500

    working

--- lib/ui/custom_widgets/custom_text_filed.dart
+++ lib/ui/custom_widgets/custom_text_filed.dart
@@ -11,44 +11,96 @@ class CustomTextFiled extends StatefulWidget {
       {required this.mcontroller,
       required this.mfillcolor,
       required this.micon,
-      required this.isPassword
-      });
+      required this.isPassword});
 
   @override
   State<CustomTextFiled> createState() => _CustomTextFiledState();
 }
 
 class _CustomTextFiledState extends State<CustomTextFiled> {
+  bool passVisible = false;
+
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      controller: widget.mcontroller,
-      cursorColor: Theme.of(context).brightness==Brightness.light ? MyColors.textWColor : MyColors.textBColor,
-      style: mTextStylr16(
-          mColor: Theme.of(context).brightness == Brightness.light
-              ? MyColors.textWColor
-              : MyColors.textBColor),
-      decoration: InputDecoration(
-          prefixIcon: Icon(widget.micon),
-          iconColor: Theme.of(context).backgroundColor,
-          prefixIconColor: Theme.of(context).backgroundColor,
-          fillColor: widget.mfillcolor,
-          filled: true,
-          focusColor: Theme.of(context).backgroundColor,
-          enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                  width: 1,
-                  color: Theme.of(context).brightness==Brightness.light ? MyColors.textBColor : MyColors.textWColor
-              ),
-              borderRadius: BorderRadius.circular(21)),
-      focusedBorder: OutlineInputBorder(
-        borderSide: BorderSide(
-          width: 1,
-          color: Theme.of(context).brightness==Brightness.light ? MyColors.textWColor : MyColors.textBColor
-        ),
-        borderRadius: BorderRadius.circular(21)
-      )
-      ),
-    );
+    return widget.isPassword
+        ? TextField(
+            obscuringCharacter: '*',
+            obscureText: !passVisible,
+            controller: widget.mcontroller,
+            cursorColor: Theme.of(context).brightness == Brightness.light
+                ? MyColors.textWColor
+                : MyColors.textBColor,
+            style: mTextStylr16(
+                mColor: Theme.of(context).brightness == Brightness.light
+                    ? MyColors.textWColor
+                    : MyColors.textBColor),
+            decoration: InputDecoration(
+                prefixIcon: Icon(widget.micon),
+                suffixIcon: Padding(
+                  padding: const EdgeInsets.only(right: 9),
+                  child: IconButton(
+                    icon: passVisible
+                        ? Icon(Icons.visibility)
+                        : Icon(Icons.visibility_off_outlined),
+                    onPressed: () {
+                      passVisible = !passVisible;
+
+                      setState(() {
+
+                      });
+                    },
+                  ),
+                ),
+                iconColor: Theme.of(context).backgroundColor,
+                prefixIconColor: Theme.of(context).backgroundColor,
+                fillColor: widget.mfillcolor,
+                filled: true,
+                focusColor: Theme.of(context).backgroundColor,
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        width: 1,
+                        color: Theme.of(context).brightness == Brightness.light
+                            ? MyColors.textBColor
+                            : MyColors.textWColor),
+                    borderRadius: BorderRadius.circular(21)),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        width: 1,
+                        color: Theme.of(context).brightness == Brightness.light
+                            ? MyColors.textWColor
+                            : MyColors.textBColor),
+                    borderRadius: BorderRadius.circular(21))),
+          )
+        : TextField(
+            controller: widget.mcontroller,
+            cursorColor: Theme.of(context).brightness == Brightness.light
+                ? MyColors.textWColor
+                : MyColors.textBColor,
+            style: mTextStylr16(
+                mColor: Theme.of(context).brightness == Brightness.light
+                    ? MyColors.textWColor
+                    : MyColors.textBColor),
+            decoration: InputDecoration(
+                prefixIcon: Icon(widget.micon),
+                iconColor: Theme.of(context).backgroundColor,
+                prefixIconColor: Theme.of(context).backgroundColor,
+                fillColor: widget.mfillcolor,
+                filled: true,
+                focusColor: Theme.of(context).backgroundColor,
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        width: 1,
+                        color: Theme.of(context).brightness == Brightness.light
+                            ? MyColors.textBColor
+                            : MyColors.textWColor),
+                    borderRadius: BorderRadius.circular(21)),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        width: 1,
+                        color: Theme.of(context).brightness == Brightness.light
+                            ? MyColors.textWColor
+                            : MyColors.textBColor),
+                    borderRadius: BorderRadius.circular(21))),
+          );
   }
 }
